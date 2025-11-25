5c074a21386ebda800355a4f812ed1b0921b2b93
siranjivikumar88 <siranjivikumar88@gmail.com>
2025-11-25 18:47:48 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./page/Home"
-import NotFound from "./page/NotFound"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Home from "./page/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import NotFound from "./page/NotFound";
+import HeroSection from "./components/HeroSection";
 
 function App() {
-
   return (
-    <>
     <BrowserRouter>
-    <Routes>
-      <Route index element={<Home/>}/>
-      <Route path='*' element={<NotFound/>}/>
-    </Routes>
+      <Routes>
+        {/* Default Page */}
+        <Route index element={<Home />} />
+
+        {/* Page Routes */}
+                <Route path="/home" element={<HeroSection />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* 404 Page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-    
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
